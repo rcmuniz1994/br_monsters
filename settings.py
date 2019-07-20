660610f23c88e6650f55ed481c78cc1f8900f68a
Rodrigo Muniz <rodrigomuniz.gppcom@gmail.com>
2019-07-20 20:48:47 -0300

    Adding the window of the game

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """
+    Armazena as configurações do jogo
+    """
+
+    def __init__(self):
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
---
+++ window.py
@@ -0,0 +1,26 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode(
+                (ai_settings.screen_width,
+                 ai_settings.screen_height))
+    pygame.display.set_caption("BR Monsters")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(ai_settings.bg_color)
+
+        pygame.display.flip()
+
+run_game()
