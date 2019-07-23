b6e62ec0a6c23866653238ee0d221c9a1d64ef96
Rodrigo Muniz <rodrigomuniz.gppcom@gmail.com>
2019-07-23 12:19:45 -0300

    Refactoring the screen update

--- game_functions.py
+++ game_functions.py
@@ -10,3 +10,11 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    # Redesenhando a tela a cada iteração
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Deixa a tela mais recente visível
+    pygame.display.flip()
--- window.py
+++ window.py
@@ -20,10 +20,6 @@ def run_game():
     while True:
         
         gf.check_events()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
