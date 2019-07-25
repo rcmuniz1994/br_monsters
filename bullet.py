03df5fbe50226ff9cc012a336295bb889f1310f9
Rodrigo Muniz <rodrigomuniz.gppcom@gmail.com>
2019-07-25 13:26:57 -0300

    Adding bullet class

---
+++ bullet.py
@@ -0,0 +1,6 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    pass
--- settings.py
+++ settings.py
@@ -4,6 +4,13 @@ class Settings():
     """
 
     def __init__(self):
+        #Screen 
         self.screen_width = 1280
         self.screen_height = 680
         self.bg_color = (200, 200, 200)
+
+        #Bullet
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
