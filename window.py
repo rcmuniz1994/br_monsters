964c6c8a7a481c96a76aef7f22a2dc152ec5d53f
wallysslima <wallysslima@gmai.com>
2019-10-07 01:38:00 -0300

    Grouping bullets

--- window.py
+++ window.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from heroes import Ship
@@ -16,10 +17,13 @@ def run_game():
                  ai_settings.screen_height))
     pygame.display.set_caption("BR Monsters")
     ship = Ship(ai_settings, screen)
+    # Criar um grupo para armazenar as balas
+    bullets = Group()
 
-    while True:   
-        gf.check_events(ship)
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
